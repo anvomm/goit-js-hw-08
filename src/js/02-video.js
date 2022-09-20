101b467837b4a4e9ded447889ab0419e92fa9fd8
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-20 23:56:26 +0300

    task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+    localStorage.setItem("videoplayer-current-time", evt.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
