b4c40fb1f583a1babad6cbe50c34fafade04eae0
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-21 12:29:07 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,6 +8,10 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(evt) {
     localStorage.setItem("videoplayer-current-time", evt.seconds);
+    
+    if (evt.seconds === evt.duration) {
+        localStorage.removeItem("videoplayer-current-time");
+    }
 }
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
